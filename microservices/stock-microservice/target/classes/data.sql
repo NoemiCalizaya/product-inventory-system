6a642659c3a17742d7ffa83b637f20a855bd7d1b
NoemiCalizaya <calizayanoemiisabel@gmail.com>
2025-10-10 22:56:27 -0400

    Mis cambios locales antes del cherry-pick

---
+++ microservices/stock-microservice/target/classes/data.sql
@@ -0,0 +1,9 @@
+-- Datos de ejemplo para el microservicio de stock
+-- Este archivo se ejecutará automáticamente al iniciar la aplicación
+
+INSERT INTO stock (code, quantity) VALUES 
+('PROD001', 100),
+('PROD002', 50),
+('PROD003', 25),
+('PROD004', 0),
+('PROD005', 3);
